1adf6a77bda1ce68443ac2603021e935e16bc5fa
Lucas <devglucas@gmail.com>
2026-06-02 20:31:24 -0300

    initial config

---
+++ next-saas-rbac/config/eslint-config/base.js
@@ -0,0 +1,13 @@
+/** @type {import("eslint").Linter.Config} */
+
+module.exports = {
+  extends: [
+      "@rocketseat/eslint-config/react"
+  ],
+  plugins: [
+      "simple-import-sort"
+  ],
+  rules: {
+      "simple-import-sort/imports": "error"
+  }
+}
